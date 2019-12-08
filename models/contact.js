65877dd28422796514cf11c450e1392a2720ab98
Sukh <sukhpartap.singh@techcanary.com>
2019-12-08 15:38:03 -0500

    Connected Database and created contact model

---
+++ models/contact.js
@@ -0,0 +1,14 @@
+let mongoose = require('mongoose');
+
+// create a model class
+let contactSchema = mongoose.Schema({
+   FirstName : String,
+   LastName : String,
+   Age : Number
+},
+{
+    collection:"First"
+});
+
+
+module.exports = mongoose.model('Test',contactSchema)
---
+++ routes/contact.js
@@ -0,0 +1,25 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// create a reference to the db schema
+let contact = require('../models/contact')
+
+/* GET Contact List  */
+router.get('/', (req,res,next) => {
+    contact.find((err,contactList) => {
+        if(err) {
+            return console.error(err);
+        }
+        else {
+            console.log(contactList);
+            /*
+            res.render('contacts/index',{
+                title:'Contact List',
+                contactList : contactList
+            });
+            */
+        }
+    });
+});
+module.exports = router;
