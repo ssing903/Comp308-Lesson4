f3e65b4a9cf1e4ba90fc4a5f544da26fdc32c83d
Sukh <sukhpartap.singh@techcanary.com>
2019-12-09 19:48:09 -0500

    Added Contact Table

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "URI":"mongodb://localhost/Test"
+    //"URI":"mongodb://localhost/Test"
+    "URI":"mongodb://sukh:sukh123@ds044989.mlab.com:44989/comp308-2019-lesson4"
 }
--- routes/contact.js
+++ routes/contact.js
@@ -13,12 +13,10 @@ router.get('/', (req,res,next) => {
         }
         else {
             console.log(contactList);
-            /*
             res.render('contacts/index',{
                 title:'Contact List',
                 contactList : contactList
             });
-            */
         }
     });
 });
